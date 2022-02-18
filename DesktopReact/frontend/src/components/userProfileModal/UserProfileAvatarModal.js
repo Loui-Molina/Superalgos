9cfceb443a2e25c7229719b09012402d3d211ceb
Claudio <claudiosuperalgos@gmail.com>
2022-02-18 11:15:26 -0300

    [Desktop] Implemented destructured avatar editor component Updated props

--- DesktopReact/frontend/src/components/userProfileModal/UserProfileAvatarModal.js
+++ DesktopReact/frontend/src/components/userProfileModal/UserProfileAvatarModal.js
@@ -1,28 +1,81 @@
-import React, {useState} from 'react';
+import React from 'react';
 import AvatarEditor from "react-avatar-editor";
-import ReactAvatarEditor from 'react-avatar-editor';
-import {Modal} from "@mui/material";
+import {Box, Button, IconButton, Modal, Typography} from "@mui/material";
+import {CloseOutlined, ZoomInOutlined, ZoomOutOutlined} from "@mui/icons-material";
 
-// this will be used in the future, on destructuring.
-const UserProfileAvatarModal = (props) => {
+const UserProfileAvatarModal = ({ modalEditAvatar,
+                                    modalEditBanner,
+                                    avatarEditorClose,
+                                    handleNewImage,
+                                    avatarEditor,
+                                    bannerEditor,
+                                    handlePositionChange,
+                                    setEditorRef,
+                                    handleScale
+                                }) => {
 
-    const {
-        userInfo,
-        handleChange,
-        selectProfilePic,
-        selectBannerPic,
-        saveProfile,
-        isEquals,
-        errorState,
-        close,
-        modalEditAvatar
-    } = props;
+    const modalEditAvatarHeader = () => {
+        return (<>
+            <IconButton onClick={avatarEditorClose}>
+                <CloseOutlined/>
+            </IconButton>
+            <Typography>
+                Edit media
+            </Typography>
+            <Button variant="outlined"
+                    className="editProfileAvatarHeaderApplyButton"
+                    onClick={handleNewImage}>
+                Apply
+            </Button>
+        </>)
+    }
 
-    return (
-        <div>
+    const modalEditAvatarBody = () => {
+        return (<>
+            <AvatarEditor className="reactAvatarEditor"
+                          image={modalEditBanner ? bannerEditor.image : avatarEditor.image}
+                          width={modalEditBanner ? bannerEditor.width : avatarEditor.width}
+                          height={modalEditBanner ? bannerEditor.height : avatarEditor.height}
+                          borderRadius={modalEditBanner ? bannerEditor.borderRadius : avatarEditor.borderRadius}
+                          scale={parseFloat(modalEditBanner ? bannerEditor.scale : avatarEditor.scale)}
+                          position={modalEditBanner ? bannerEditor.position : avatarEditor.position}
+                          onPositionChange={handlePositionChange}
+                          ref={setEditorRef}
+            />
+        </>)
+    }
 
-        </div>
+    const modalEditAvatarFooter = () => {
+        return (<>
+            <ZoomOutOutlined/>
+            <input className="editProfileAvatarFooterZoom"
+                   name="scale"
+                   type="range"
+                   onChange={handleScale}
+                   min={modalEditBanner ? bannerEditor.allowZoomOut ? '0.1' : '1' : avatarEditor.allowZoomOut ? '0.1' : '1'}
+                   max="2"
+                   step="0.01"
+                   defaultValue="1"
+            /><ZoomInOutlined/>
+        </>)
+    }
 
+    return (<>
+            <Modal open={modalEditAvatar}
+                   onClose={avatarEditorClose}>
+                <Box className="editProfileAvatar">
+                    <div className="editProfileAvatarHeader">
+                        {modalEditAvatarHeader()}
+                    </div>
+                    <div className="editProfileAvatarBody">
+                        {modalEditAvatarBody()}
+                    </div>
+                    <div className="editProfileAvatarFooter">
+                        {modalEditAvatarFooter()}
+                    </div>
+                </Box>
+            </Modal>
+        </>
     );
 };
 
